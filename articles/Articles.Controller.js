0dd9703a337215cca0e076a46dba01bc45d8e2c4
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2024-07-10 16:11:20 -0300

    formulario de novo artigo criado

--- articles/Articles.Controller.js
+++ articles/Articles.Controller.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../categories/Category');
 
 router.get('/articles', (req, res) => {
   res.send('Rota de Artigos');
 });
 
-router.get('/admin/articles/new', (req, res) => {
-  res.render('./admin/articles/new');
+router.get('/admin/articles/new', async (req, res) => {
+  const categories = await Category.findAll();
+
+  res.render('./admin/articles/new', { categories });
 });
 
 module.exports = router;
