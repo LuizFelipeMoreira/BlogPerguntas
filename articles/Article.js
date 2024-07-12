12e4b63b2b0e3c1e3c18ed1ca88f90d12a5f386c
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2024-07-12 12:29:57 -0300

    salvando artigos e ajustando timezone

--- articles/Article.js
+++ articles/Article.js
@@ -8,7 +8,7 @@ const Article = connection.define('article', {
   body: { type: Sequelize.TEXT, allowNull: false },
 });
 
-Category.hasMany(Article);
-Article.belongsTo(Category);
+Category.hasMany(Article); //Uma categoria tem muitos artigos
+Article.belongsTo(Category); // Um artigo pertence a uma categoria
 
 module.exports = Article;
--- articles/Articles.Controller.js
+++ articles/Articles.Controller.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('../categories/Category');
+const Article = require('./Article');
+const slugify = require('slugify');
 
-router.get('/articles', (req, res) => {
+router.get('/admin/articles', (req, res) => {
   res.send('Rota de Artigos');
 });
 
@@ -10,6 +12,23 @@ router.get('/admin/articles/new', async (req, res) => {
   const categories = await Category.findAll();
 
   res.render('./admin/articles/new', { categories });
+});
+
+router.post('/articles/save', async (req, res) => {
+  const { title, body, category } = req.body;
+
+  try {
+    await Article.create({
+      title,
+      slug: slugify(title),
+      body,
+      categoryId: category,
+    });
+
+    res.redirect('/admin/articles');
+  } catch (error) {
+    res.send(`<h1>${error}</h1>`);
+  }
 });
 
 module.exports = router;
